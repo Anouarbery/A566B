12888d7c90c7cb7dce351b7b9a22772746b28b9d
George Kalinin <g.kalinin07@proton.me>
2025-06-12 12:14:02 +0000

    a56x: Initial TWRP device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_a56x.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_a56x-user \
+    twrp_a56x-userdebug \
+    twrp_a56x-eng
