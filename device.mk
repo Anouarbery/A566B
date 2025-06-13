7c74bd66abdbe1aa28a13344fb6b23a307535b24
George Kalinin <g.kalinin07@proton.me>
2025-06-13 17:03:51 +0000

    a56x: update device.mk

--- device.mk
+++ device.mk
@@ -6,11 +6,21 @@
 #
 
 LOCAL_PATH := device/samsung/a56x
+
+# Enable virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# Enable developer GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
@@ -20,13 +30,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.erd8855
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.erd8855 \
-    libgptutils \
-    libz \
-    libcutils
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -34,3 +39,30 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Recovery additional binaries
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libxml2
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
+
+# Shipping level
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Vendor Boot Platform
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/platform/fstab.s5e8855:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.s5e8855 \
+    $(LOCAL_PATH)/prebuilt/platform/fstab.s5e8855:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/fstab.s5e8855 \
+    $(LOCAL_PATH)/prebuilt/platform/mgfx2.0_mid.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/lib/firmware/sgpu/mgfx2.0_mid.bin \
+    $(LOCAL_PATH)/prebuilt/platform/mgfx2.0_mid.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/firmware/sgpu/mgfx2.0_mid.bin \
+    $(LOCAL_PATH)/prebuilt/platform/gt9916k_a56x.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/vendor/firmware/tsp/gt9916k_a56x.bin
