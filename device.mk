bd7c28bc013416a16c3758d16fd075966c39e191
Anouarbery <sirinaberriri@gmail.com>
2025-08-11 20:08:06 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -16,7 +15,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Configure emulated_storage.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
@@ -25,13 +23,16 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-exynos \
-    android.hardware.boot@1.2-impl-exynos.recovery \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
     android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+    fastbootd 
+
+PRODUCT_PACKAGES += \
+    bootctrl.erd8855
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -43,26 +44,13 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Recovery additional binaries
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libion \
-    libxml2
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
-
 # Shipping level
-PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_SHIPPING_API_LEVEL := 32
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# Vendor Boot Platform
+# Decryption
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/platform/fstab.s5e8855:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.s5e8855 \
-    $(LOCAL_PATH)/prebuilt/platform/fstab.s5e8855:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/fstab.s5e8855 \
-    $(LOCAL_PATH)/prebuilt/platform/mgfx2.0_mid.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/lib/firmware/sgpu/mgfx2.0_mid.bin \
-    $(LOCAL_PATH)/prebuilt/platform/mgfx2.0_mid.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/firmware/sgpu/mgfx2.0_mid.bin \
-    $(LOCAL_PATH)/prebuilt/platform/gt9916k_a56x.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/vendor/firmware/tsp/gt9916k_a56x.bin
+    $(LOCAL_PATH)/prebuilt/platform/openrecoveryscript:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/cache/recovery/openrecoveryscript
